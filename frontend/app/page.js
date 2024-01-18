ce6b7f078c38c4208d8b753100e44ad736b05e62
nunofds <fernandes.n.sa@outlook.fr>
2024-01-18 15:50:26 +0100

    correct next/babel on .eslintrc.json file

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const page = () => {
+const Page = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Home page</h1>
