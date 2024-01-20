193ccd21b5e4c168f2e808a8553cc875fcb69476
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-01-20 12:16:38 +0100

    none

---
+++ backend/routes/sales.routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const sales = require("./../models/Sales.model");
+
+// GET all sales
+
+// Create a new sale
+
+// Update a sale with id
+
+// Delete a sale with id
+
+// Update a sale
+
+module.exports = router;
