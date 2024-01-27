52408629b91cc229efaef01007000d8559bb8e81
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-01-27 13:08:11 +0100

    footer and navbar components created

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavBar from "./Components/NavBar.jsx";
 
 const Page = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Home page</h1>
+            <NavBar />
         </main>
     );
 };
