78169b3c060c96d6a9e1583d87950085127bf554
nunofds <fernandes.n.sa@outlook.fr>
2024-01-30 22:50:58 +0100

    commit

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Home page</h1>
+            <h1>ChicCloset</h1>
         </main>
     );
 };
