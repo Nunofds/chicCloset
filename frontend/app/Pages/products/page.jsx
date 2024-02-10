089a653b66c32959fe04241e7f0c7f5c01638ae8
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-02-10 15:36:15 +0100

    push frontend

--- frontend/app/Pages/products/page.jsx
+++ frontend/app/Pages/products/page.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const page = () => {
-    return <div></div>;
+const OneProductDetail = (props) => {
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        const fetchProductDetails = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5000/products/${props.productId}`);
+                setProduct(response.data);
+            } catch (error) {
+                console.error("Error fetching product details:", error);
+                setProduct(null);
+            }
+        };
+
+        fetchProductDetails();
+    }, [props.productId]);
+
+    if (!product) {
+        return <p>Loading...</p>; // Vous pouvez également afficher un message d'erreur si le produit n'est pas trouvé.
+    }
+
+    return (
+        <div>
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <p>
+                Price: {product.price.value} {product.price.currency[0]}
+            </p>
+            {/* Ajoutez d'autres détails du produit selon vos besoins */}
+        </div>
+    );
 };
 
-export default page;
+export default OneProductDetail;
