d235ddfa9d7b0834f46f08ab47a130bb5fab1518
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-02-10 15:31:23 +0100

    deleted files and added news files front end

---
+++ frontend/app/Pages/products/[id].jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import OneProductDetail from "../../components/OneProductDetail";
+import { useRouter } from "next/router";
+
+const ProductPage = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return (
+        <div>
+            <h1>Product Page</h1>
+            {id && <OneProductDetail productId={id} />}
+        </div>
+    );
+};
+
+export default ProductPage;
