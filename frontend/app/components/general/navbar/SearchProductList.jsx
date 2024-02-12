fa62ffa4a661769132b05d99c571346df6c7c989
nunoF <fernandes.n.sa@outlook.fr>
2024-02-12 12:19:55 +0100

    compare with other pull

--- frontend/app/components/general/navbar/SearchProductList.jsx
+++ frontend/app/components/general/navbar/SearchProductList.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import SearchResult from "./SearchResult";
-
-const ProductList = ({ results }) => {
-    console.log("Results in ProductList:", results);
-    return (
-        <div className="h-[40] w-[100] ">
-            {results.map((result) => {
-                return <SearchResult product={result} key={result._id} />;
-            })}
-        </div>
-    );
-};
-
-export default ProductList;
-=======
 import React from "react";
 import SearchResult from "./SearchResult";
 
@@ -35,4 +18,3 @@ const ProductList = ({ results, selectedCategory }) => {
 };
 
 export default ProductList;
->>>>>>> nuno--frontend
--- frontend/app/components/general/navbar/SearchResult.jsx
+++ frontend/app/components/general/navbar/SearchResult.jsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Link from "next/link";
-
-function SearchResult({ product }) {
-    console.log("Product in SearchResult:", product);
-    return (
-        <div>
-            {product ? (
-                <div>
-                    {/* ici creer route pour afficher les produits rechercher dans la navbar */}
-                    <Link href={`/product/${product.id}`}>
-                        <div className="hover:bg-gray-100">{product.name}</div>
-                    </Link>
-                </div>
-            ) : (
-                <p>No results found</p>
-            )}
-        </div>
-    );
-}
-
-export default SearchResult;
-=======
 "use client";
 import React, { useEffect } from "react";
 import Link from "next/link";
@@ -52,4 +28,3 @@ function SearchResult({ product }) {
 }
 
 export default SearchResult;
->>>>>>> nuno--frontend
