0e84b65c6bbafb11e616e6f5624ea54105d4215b
nunoF <fernandes.n.sa@outlook.fr>
2024-02-12 08:02:45 +0100

    continue updated folders

--- frontend/app/(Pages)/auth/login/page.jsx
+++ frontend/app/(Pages)/auth/login/page.jsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
-    return <div></div>;
+    return (
+        <div>
+            <div>
+                <Link href={"/account/dashboard"}>dashboard</Link>
+            </div>
+
+            <div>
+                <Link href={"/cart/wishlist"}>wishlist</Link>
+            </div>
+
+            <div>
+                <Link href={"/cart"}>Cart</Link>
+            </div>
+        </div>
+    );
 }
