e4fcc6bb52e4f17a03d1fd9b447599936999aa0d
nunoF <fernandes.n.sa@outlook.fr>
2024-02-12 12:21:49 +0100

    delete folders that we don't need

--- frontend/app/Pages/user/dashboard/cart/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cart = () => {
-    return <div>Mon Panier</div>;
-};
-
-export default Cart;
--- frontend/app/Pages/user/dashboard/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-    return <div>User Dashboard</div>;
-};
-
-export default Page;
